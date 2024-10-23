322351a6dee2fdbe7961762ae9d75a6e68a420a5
pranavkodin <sunny.pranav2006@gmail.com>
2024-10-23 16:42:53 +0530

    disaster happened many things done mainly files are organized

---
+++ static/js/script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', function () {
+        var hoverSound = document.getElementById('hoverSound');
+        hoverSound.play();
+    });
+});
+
+document.querySelectorAll('.socialmediaico i').forEach(icon => {
+    icon.addEventListener('mouseenter', function () {
+        var hoverSound = document.getElementById('socialmediahoverSound');
+        hoverSound.play();
+    });
+});
+const playSound = () => {
+    const hoverSound = document.getElementById('buttonclickSound');
+    hoverSound.currentTime = 0; // Reset to start
+    hoverSound.play();
+};
+
+document.querySelectorAll('.socialmediaico i, button').forEach(element => {
+    element.addEventListener('click', playSound);
+});
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
+
+var btn = $('check');
+
